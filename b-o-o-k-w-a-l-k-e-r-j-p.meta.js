bc346c2c18d9b525f5965f03351967db274f67bb
orysinquis <orysinquis@github.com>
2024-10-17 12:59:52 +0700

    up

--- b-o-o-k-w-a-l-k-e-r-j-p.meta.js
+++ b-o-o-k-w-a-l-k-e-r-j-p.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b-o-o-k-w-a-l-k-e-r-j-p
 // @namespace    https://github.com/orysinquis/loi_phong_ba/raw/main/b-o-o-k-w-a-l-k-e-r-j-p.user.js
-// @version      2024.08.20.1
+// @version      2024.10.17
 // @description  down b-o-o-k-w-a-l-k-e-r-j-p
 // @author       You
 // @include      /^https:\/\/vi[e3]w[e3]r\.b[o0][o0]kw[a@]lker\.j[p9]\x2F.+/
