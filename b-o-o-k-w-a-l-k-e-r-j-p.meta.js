3dff1a46a99176a2faa9de346a602168c303d593
orysinquis <orysinquis@github.com>
2025-02-22 15:22:48 +0700

    up

--- b-o-o-k-w-a-l-k-e-r-j-p.meta.js
+++ b-o-o-k-w-a-l-k-e-r-j-p.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         b-o-o-k-w-a-l-k-e-r-j-p
 // @namespace    https://github.com/orysinquis/loi_phong_ba/raw/main/b-o-o-k-w-a-l-k-e-r-j-p.user.js
-// @version      2024.10.17
+// @version      2025.02.22
 // @description  down b-o-o-k-w-a-l-k-e-r-j-p
 // @author       You
-// @include      /^https:\/\/vi[e3]w[e3]r\.b[o0][o0]kw[a@]lker\.j[p9]\x2F.+/
+// @include      /^https:\/\/vi[e3]w[e3]r(?:-tr[i1][a4]l)?\.b[o0][o0]kw[a@]lker\.j[p9]\x2F.+/
 // @icon         https://raw.githubusercontent.com/orysinquis/loi_phong_ba/main/b-o-o-k-w-a-l-k-e-r-j-p.png
 // @grant        GM_download
 // @grant        GM_info
